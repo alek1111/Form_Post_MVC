8834c8e5a0010c2eaad2ee42a9ed25a08c4748d9
alek1111 <32782315+alek1111@users.noreply.github.com>
2022-06-22 14:25:15 -0500

    Add project files.

---
+++ Form_Post_MVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Form_Post_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Post_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonModel person)
+        {
+            if (ModelState.IsValid)
+            {
+                int personId = person.PersonId;
+                string name = person.Name;
+                string gender = person.Gender;
+                string city = person.City;
+            }
+            return View();
+        }
+    }
+}
---
+++ Form_Post_MVC/Models/PersonModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Form_Post_MVC.Models
+{
+    public class PersonModel
+    {
+        /// <summary>
+        /// Gets or sets PersonId.
+        /// </summary>
+        [Range(1000, 2000)]
+        public int PersonId { get; set; }
+
+        /// <summary>
+        /// Gets or sets Name.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Gender.
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets City.
+        /// </summary>
+        public string City { get; set; }
+    }
+}
