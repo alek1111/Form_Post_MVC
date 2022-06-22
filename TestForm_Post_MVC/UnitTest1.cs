421a177ca20908f2c8dc0de7c881efd48dade42c
alek1111 <32782315+alek1111@users.noreply.github.com>
2022-06-22 15:50:22 -0500

    commit

---
+++ TestForm_Post_MVC/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestForm_Post_MVC
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
